a74c9f3bfa7d0ca8a367dc876c8e5449f4bc1cfa
kapakrystian <56194438+kapakrystian@users.noreply.github.com>
2024-06-12 19:13:32 +0200

    Lesson 42 Login Form Refactoring

--- PHP For Beginners/Http/controllers/session/store.php
+++ PHP For Beginners/Http/controllers/session/store.php
@@ -2,27 +2,23 @@
 
 use Core\App;
 use Core\Database;
-use Core\Validator;
+use Http\Forms\LoginForm;
 
 $db = App::resolve(Database::class);
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$errors = [];
-if (!Validator::email($email)) {
-    $errors['email'] = 'Please provide a valid email address.';
-}
 
-if (!Validator::string($password)) {
-    $errors['password'] = 'Please provide a valid password.';
-}
+$form = new LoginForm();
 
-if (!empty($errors)) {
+
+if (!$form->validate($email, $password)) {
     return view('session/create.view.php', [
-        'errors' => $errors
+        'errors' => $form->errors()
     ]);
 }
+
 
 $user = $db->query('select * from users where email = :email', [
     'email' => $email
@@ -39,6 +35,7 @@ if ($user) {
         exit();
     }
 }
+
 
 return view('session/create.view.php', [
     'errors' => [
