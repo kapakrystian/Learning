ae14f4163166d097cc33344d039ea2718a19ed98
kapakrystian <56194438+kapakrystian@users.noreply.github.com>
2024-06-18 21:07:53 +0200

    Lesson 46

--- PHP For Beginners/Http/controllers/session/store.php
+++ PHP For Beginners/Http/controllers/session/store.php
@@ -2,34 +2,23 @@
 
 
 use Core\Authenticator;
-use Core\Session;
 use Http\Forms\LoginForm;
 
 
-$email = $_POST['email'];
-$password = $_POST['password'];
-
-
-$form = new LoginForm();
-
-
-if ($form->validate($email, $password)) {
-
-    $auth = new Authenticator();
-
-    if ($auth->attempt($email, $password)) {
-        redirect('/');
-    }
-
-    $form->error('email', 'No matching account found for that email address and password');
-}
-
-
-Session::flash('errors', $form->errors());
-Session::flash('old', [
-    'email' => $email,
-    'password' => $password
+$form = LoginForm::validate($attributes = [
+    'email' => $_POST['email'],
+    'password' => $_POST['password']
 ]);
 
+
+$auth = new Authenticator();
+$signedIn = $auth->attempt($attributes['email'], $attributes['password']);
+
+if (!$signedIn) {
+    $form->error(
+        'email',
+        'No matching account found for that email address and password'
+    )->throw();
+}
 
 return redirect('/login');
