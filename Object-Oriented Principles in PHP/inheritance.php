a7a556576d8b75cf737793e106a679c8ecf48ef3
kapakrystian <56194438+kapakrystian@users.noreply.github.com>
2024-07-08 19:35:35 +0200

    OOP part 2 - inheritance

--- Object-Oriented Principles in PHP/inheritance.php
+++ Object-Oriented Principles in PHP/inheritance.php
@@ -1,3 +1,50 @@
 <?php
 
 
+class Collection
+{
+
+    protected array $items;
+
+
+    public function __construct(array $items)
+    {
+        $this->items = $items;
+    }
+
+
+    public function sum($key)
+    {
+        return array_sum(array_column($this->items, $key));
+    }
+}
+
+
+class VideoCollection extends Collection
+{
+    public function length()
+    {
+        return $this->sum('length');
+    }
+}
+
+
+class Video
+{
+    public $title;
+    public $length;
+
+    public function __construct($title, $length)
+    {
+        $this->title = $title;
+        $this->length = $length;
+    }
+}
+
+$videos = new VideoCollection([
+    new Video('Rambo', 130),
+    new Video('Rambo 2', 120),
+    new Video('Rambo 3', 145),
+]);
+
+var_dump($videos->length());
